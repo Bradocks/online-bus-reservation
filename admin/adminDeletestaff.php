11367adaa9d212f9c217e6213e1b8737f24d2ba3
Bradocks <98581615+Bradocks@users.noreply.github.com>
2024-05-05 10:59:34 +0300

    Update adminDeletestaff.php

--- admin/adminDeletestaff.php
+++ admin/adminDeletestaff.php
@@ -1,30 +1,30 @@
 <?php
-// Start a session to manage user
+// Start a session to manage the user
 session_start(); 
 //estalish connnection 
 $servername="localhost";
-$username="Newton";
-$password="database@123";
-$dbname="parceldelivery";
+$username="root";
+$password="root";
+$dbname="busreservation";
 $conn=new mysqli($servername,$username,$password,$dbname); // using the php new mysqli function to establish a connection.
 
-/* Check if the connection to the database was successful usinh connect_error if there is an connect_error the error will be 
-displayed and execution will be terminited by the die function. */
+/* Check if the connection to the database was successful using connect_error if there is a connect_error the error will be 
+displayed and execution will be terminated by the die function. */
  if ($conn->connect_error)  {
-       die ("connection failled:". $conn->connect_error); // Terminate script execution if the connection fails
+       die ("connection failed:". $conn->connect_error); // Terminate script execution if the connection fails
         
 }  
 
 $staffId=$_POST['staffId'];
 
-$check="select staffId from staff where staffId=$staffId";
+$check=" select staffId from staff where staffId=$staffId";
 /*executes the SQL query using the query() method of the database connection 
        object $conn. it sends the SQL query to the database server for execution.*/
  $checkResult=$conn->query($check);
 if($checkResult->num_rows>0) { 
 
 $sql= "DELETE FROM staff WHERE staffId=?";
-// a  query that deletes values using placholders ?
+// a  query that deletes values using placeholders?
 $stmt=$conn->prepare($sql);
 //The bind_param() method binds variables to the placeholders in the SQL query.
 $stmt->bind_param("i",$staffId);
