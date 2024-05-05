ae2ca97cf3ffb0406bca60506f30ebc25104a4db
Bradocks <98581615+Bradocks@users.noreply.github.com>
2024-05-05 16:57:54 +0300

    Update AdminDeleteVehicle.php

--- admin/AdminDeleteVehicle.php
+++ admin/AdminDeleteVehicle.php
@@ -1,23 +1,23 @@
 <?php
-// Start a session to manage user
+// Start a session to manage the user
 session_start(); 
 //estalish connnection 
 $servername="localhost";
-$username="Newton";
-$password="database@123";
-$dbname="parceldelivery";
+$username="root";
+$password="root";
+$dbname="busreservation";
 $conn=new mysqli($servername,$username,$password,$dbname); // using the php new mysqli function to establish a connection.
 
-/* Check if the connection to the database was successful usinh connect_error if there is an connect_error the error will be 
-displayed and execution will be terminited by the die function. */
+/* Check if the connection to the database was successful using connect_error if there is a connect_error the error will be 
+displayed and execution will be terminated by the die function. */
  if ($conn->connect_error)  {
-       die ("connection failled:". $conn->connect_error); // Terminate script execution if the connection fails
+       die ("connection failed:". $conn->connect_error); // Terminate script execution if the connection fails
         
 }  
 
 $vehicleId=$_POST['vehicleId'];
 
-$check="select vehicleId from vehicle where vehicleId=$vehicleId";
+$check=" select vehicleId from vehicle where vehicleId=$vehicleId";
 /*executes the SQL query using the query() method of the database connection 
 object $conn. it sends the SQL query to the database server for execution.*/
  $checkResult=$conn->query($check);
@@ -32,14 +32,14 @@ $stmt->bind_param("i",$vehicleId);
        provided parameter values, returns true or false */
 $stmt->execute();
 if($stmt){
-    echo" deleted sucessfully... delete another one: <a href='AdminDeleteVehicle.html'>delete another</a>" . "<br>";
+    echo" deleted sucessfully <a href='AdminDeleteVehicle.html'>delete another</a>" . "<br>";
       
-    echo" or go back to dashBoard: <a href='AdminDashboard.php'>Dashboard</a>". "<br>";
+    echo" Back to dashBoard: <a href='AdminDashboard.php'>Dashboard</a>". "<br>";
 }
 else{
-    echo"Not deleted: <a href='AdmindeleteStaff.html'>delete again</a>". "<br>";
+    echo"Not deleted: <a href='AdmindeleteStaff.html'>delete</a>". "<br>";
 }
 }else {
-    echo" vehicle not available for delete..go back to DashBoard: <a href='AdminDashboard.php'>Dashboard</a>". "<br>";
+    echo" vehicle not available, back to DashBoard: <a href='AdminDashboard.php'>Dashboard</a>". "<br>";
 }
 $conn->close();
